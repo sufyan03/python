3d3631333ad9f2c203f2188fb97f1d43daee0b24
faizan <faizanshaikh0342@gmail.com>
2020-01-04 13:14:03 +0500

    phyton

---
+++ assign 6.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
+class car:
+    def car_name(self):
+        print("Car Name : MARK X")
+    def models(self):
+        print("2018")
+    def car_detail(self):
+        print("car move 20km at 2 liter only")
+    def car_engine(self):
+        print("engine first class dual turbo xxx842")
+    def car_detail(self):
+        print("car move 20km at 2 liter only")    
+
+model = car()
+color = car()
+name = car()
+engine = car()
+detail = car()
+
+
+car.car_name(name)
+car.models(model)
+car.car_detail(detail)
+car.car_engine(engine)
+
+
+# In[10]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
